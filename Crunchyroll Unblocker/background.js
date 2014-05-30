c826dd2adcc1083a5285630b9e80a936d48c80c8
jerryteps <jerry_teps@hotmail.com>
2014-05-31 05:24:48 +1000

    v.8 RC
    Uses cookies grabbed by a webserver rather than using a proxy to get a
    cookiie

--- Crunchyroll Unblocker/background.js
+++ Crunchyroll Unblocker/background.js
@@ -3,7 +3,7 @@ var config = {
 	rules: {
 		proxyForHttp: {
 			scheme: "http",
-			host: "151.200.170.146"
+			host: "198.7.58.81"
 		}
 	}
 };
@@ -12,26 +12,19 @@ var proxyOff = {
 	mode: "direct"
 }
 
-//var _ip = chrome.storage.sync.get(ip, function () {});
-
-var opt = {
-	type: "basic",
-	title: "Cruncyroll Unblocker",
-	message: "Successfully loaded US page",
-	iconUrl: "crunblock16.png"
+function setCookie () {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			chrome.cookies.remove({url: "http://crunchyroll.com/", name: "sess_id"});
+			chrome.cookies.set({url: "http://.crunchyroll.com/", name: "sess_id", value: xhr.responseText})
+			alert(xhr.responseText);
+		}
+	}
+	xhr.open('GET', 'http://www.crunblocker.com/sess_id.php', true);
+	xhr.send(null);
 }
 
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.cookies.remove({url: "http://crunchyroll.com/", name: "sess_id"});
-
-	chrome.proxy.settings.set({value: config, scope: 'regular'}, function () {});
-
-	chrome.tabs.create({url: "http://www.crunchyroll.com"});
-	
-	alert("fdgdfgdfg ");
-
-	setTimeout(function(){chrome.proxy.settings.set({value: proxyOff, scope: 'regular'}, function () {})}, 1000);
-	
-	//chrome.notifications.create("proxysuccess", opt, function ());
-}); 
+	setCookie ();
+});
